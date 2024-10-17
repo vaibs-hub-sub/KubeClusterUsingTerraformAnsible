42035fecbeef6a313c6f11b0016a29cc06751d8a
sahil7879 <43738848+sahil7879@users.noreply.github.com>
2024-10-17 16:51:47 +0530

    Update mainec2.tf

--- mainec2.tf
+++ mainec2.tf
@@ -1,31 +1,48 @@
-variable "elb-names" {
-  type = list
-  default = ["kuber-master", "kuber-w1","Kuber-w2"]
-}
-
-variable "list" {
-  type = list
-  default = ["t2.medium","t2.medium","t2.medium"]
-}
-
-
 provider "aws" {
   region     = "ap-south-1"
-#  version = "5.60.0"
-# access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-# secret_key = "PUT-YOUR-SECRET-KEY-HERE"
 }
 
-resource "aws_instance" "kube" {
+resource "aws_instance" "master" {
 ami = "ami-0dee22c13ea7a9a67"
-count= 3
 
 # SSH key pair
   key_name = "awskp"
 
-   instance_type = var.list[count.index]
+   instance_type = t2.medium
 tags= {
-Name= var.elb-names[count.index]
+Name= kube-m
+}
+# Block device mapping for the EBS volume
+  root_block_device {
+    volume_size = 20  # Size in GB
+    volume_type = "gp2"  # General Purpose SSD
+  }
+}
+resource "aws_instance" "worker1" {
+ami = "ami-0dee22c13ea7a9a67"
+
+# SSH key pair
+  key_name = "awskp"
+
+   instance_type = t2.medium
+tags= {
+Name= kube-w1
+}
+# Block device mapping for the EBS volume
+  root_block_device {
+    volume_size = 20  # Size in GB
+    volume_type = "gp2"  # General Purpose SSD
+  }
+}
+resource "aws_instance" "worker2" {
+ami = "ami-0dee22c13ea7a9a67"
+
+# SSH key pair
+  key_name = "awskp"
+
+   instance_type = t2.medium
+tags= {
+Name= kube-w2
 }
 # Block device mapping for the EBS volume
   root_block_device {
@@ -34,5 +51,7 @@ Name= var.elb-names[count.index]
   }
 }
 output "instances_ip" {
-  value = aws_instance.kube[*].public_ip
+  value = aws_instance.master.public_ip
+  value = aws_instance.worker1.public_ip
+  value = aws_instance.worker2.public_ip
 }
