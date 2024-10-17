797218e2aee09d7f6e1213b816b35ed663ca947f
sahil7879 <43738848+sahil7879@users.noreply.github.com>
2024-10-17 09:57:39 +0530

    Create mainec2.tf

---
+++ mainec2.tf
@@ -0,0 +1,32 @@
+variable "elb-names" {
+  type = list
+  default = ["Team3-kuber-master", "Team3-kuber-n1","Team3-Kuber-n2"]
+}
+
+variable "list" {
+  type = list
+  default = ["t2.medium","t2.medium","t2.medium"]
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+#  version = "5.60.0"
+# access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+# secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+}
+
+resource "aws_instance" "Team3-proj" {
+ami = "ami-0e86e20dae9224db8"
+count= 3
+
+   instance_type = var.list[count.index]
+tags= {
+Name= var.elb-names[count.index]
+}
+# Block device mapping for the EBS volume
+  root_block_device {
+    volume_size = 20  # Size in GB
+    volume_type = "gp2"  # General Purpose SSD
+  }
+}
