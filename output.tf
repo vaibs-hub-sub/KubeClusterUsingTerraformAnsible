a720226bf97c620c81a28031908cac9d8b614486
sahil7879 <43738848+sahil7879@users.noreply.github.com>
2025-05-08 14:18:48 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,21 +1,21 @@
 # outputs.tf
 
-output "master_public_ip" {
+output "master_ip" {
   description = "Public IP address of the master node"
   value       = aws_instance.master.public_ip
 }
 
-output "worker1_public_ip" {
+output "worker1_ip" {
   description = "Public IP address of Worker 1 node"
   value       = aws_instance.worker1.public_ip
 }
 
-output "worker2_public_ip" {
+output "worker2_ip" {
   description = "Public IP address of Worker 2 node"
   value       = aws_instance.worker2.public_ip
 }
 
-output "master_private_ip" {
+output "masterp_ip" {
   description = "Private IP address of the master node"
   value       = aws_instance.master.private_ip
 }
