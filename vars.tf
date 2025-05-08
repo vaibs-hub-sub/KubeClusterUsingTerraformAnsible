39e669e68af2663eee233351757be87cb4b20fc4
sahil7879 <43738848+sahil7879@users.noreply.github.com>
2025-05-08 14:17:18 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,1 +1,12 @@
+variable "region" {
+  description = "The AWS region to launch instances in"
+  type        = string
+  default     = "ap-south-2"  # Modify this with your preferred AWS region
+}
+
+variable "key_name" {
+  description = "The SSH key pair name to access the EC2 instances"
+  type        = string
+  default     = "hydra"  # Modify this with your SSH key pair
+}
 
